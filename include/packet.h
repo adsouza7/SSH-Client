252271c5fea8be89f1ee4c77cd0c66f73f55ca8c
Aaron DSouza <aaronpeter2002@gmail.com>
2025-05-15 11:22:00 -0600

    Defined packet struct

---
+++ include/packet.h
@@ -0,0 +1,33 @@
+#ifndef PACKET_H
+#define PACKET_H
+
+#include <new>
+#include <cstring>
+
+struct Packet {
+    
+    uint8_t* buffer;
+    size_t len;
+
+    Packet(uint8_t* packetBytes, size_t numBytes) {
+        
+        // Dynamically allocate enough memory for the packet
+        buffer = new (std::nothrow)uint8_t[numBytes];
+        if (!buffer) {
+            throw std::runtime_error("Packet buffer allocation failed");
+        }
+
+        memcpy(buffer, packetBytes, numBytes);
+
+        len = numBytes;
+    }
+
+    ~Packet() {
+        if (buffer) {
+            delete [] buffer;
+        }
+    }
+
+};
+
+#endif
