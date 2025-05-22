057d12028eca4c301dd4e3d11b91b1e9bc126b31
Aaron DSouza <aaronpeter2002@gmail.com>
2025-05-22 14:11:23 -0600

    renamed some function for clarity

---
+++ src/X25519.cpp
@@ -0,0 +1,62 @@
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/core_names.h>
+#include <openssl/params.h>
+#include <vector>
+#include <iostream>
+
+EVP_PKEY* generateX25519KeyPair() {
+    
+    // Create context and initialize to generate X25519 key
+    EVP_PKEY_CTX *pctx = EVP_PKEY_CTX_new_id(EVP_PKEY_X25519, nullptr);
+    if (!pctx || EVP_PKEY_keygen_init(pctx) <= 0) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+
+    // Generate key
+    EVP_PKEY *keyPair = nullptr;
+    if (EVP_PKEY_keygen(pctx, &keyPair) <= 0) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+
+    // Free keygen context
+    EVP_PKEY_CTX_free(pctx);
+ 
+    return keyPair;
+}
+
+void X25519PubKey2Bytes(EVP_PKEY* keyPair, std::vector<uint8_t>& keyBytes) {
+
+    // Get size required to store bytes
+    size_t bufferLen = 0;
+    EVP_PKEY_get_raw_public_key(keyPair, nullptr, &bufferLen);
+    
+    keyBytes.resize(bufferLen);
+
+    // Add bytes to array
+    EVP_PKEY_get_raw_public_key(keyPair, keyBytes.data(), &bufferLen);
+
+}
+
+
+EVP_PKEY* X25519Bytes2PubKey(std::vector<uint8_t>& keyBytes) {
+    
+    EVP_PKEY* key = nullptr;
+
+    key = EVP_PKEY_new_raw_public_key(EVP_PKEY_X25519,
+                                      nullptr,
+                                      keyBytes.data(),
+                                      keyBytes.size());
+
+    if (!key) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+
+    return key;
+
+}
+
+
---
+++ src/ed25519.cpp
@@ -0,0 +1,65 @@
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/core_names.h>
+#include <openssl/params.h>
+#include <vector>
+#include <iostream>
+
+void ed25519PubKey2Bytes(EVP_PKEY* key, std::vector<uint8_t>& keyBytes) {
+
+    // Get size required to store bytes
+    size_t bufferLen = 0;
+    EVP_PKEY_get_raw_public_key(key, nullptr, &bufferLen);
+    
+    keyBytes.resize(bufferLen);
+
+    // Add bytes to array
+    EVP_PKEY_get_raw_public_key(key, keyBytes.data(), &bufferLen);
+
+}
+
+
+EVP_PKEY* ed25519Bytes2PubKey(std::vector<uint8_t>& keyBytes) {
+    
+    EVP_PKEY* key = nullptr;
+
+    key = EVP_PKEY_new_raw_public_key(EVP_PKEY_ED25519,
+                                      nullptr,
+                                      keyBytes.data(),
+                                      keyBytes.size());
+
+    if (!key) {
+        ERR_print_errors_fp(stderr);
+        abort();
+    }
+
+    return key;
+
+}
+
+
+int ed25519VerifySign(EVP_PKEY* key, std::vector<uint8_t>& hash,
+    std::vector<uint8_t>& signature) {
+
+    int ret = -1;
+
+    EVP_MD_CTX* mdctx = EVP_MD_CTX_new();
+    if (!mdctx) {
+        ERR_print_errors_fp(stderr);
+        return -1;
+    }
+
+    if (EVP_DigestVerifyInit(mdctx, nullptr, nullptr, nullptr, key) != 1) {
+        EVP_MD_CTX_free(mdctx);
+        ERR_print_errors_fp(stderr);
+        return -1;
+    }
+
+    ret = EVP_DigestVerify(mdctx, signature.data(), signature.size(),
+        hash.data(), hash.size());
+
+    EVP_MD_CTX_free(mdctx);
+    return ret;
+
+}
+
