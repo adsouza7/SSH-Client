940b6b716feb004aa7ae3f5a7d1e537b31c42c6b
Aaron DSouza <aaronpeter2002@gmail.com>
2025-05-31 11:43:24 -0600

    User inputs usernames and password

--- src/ssh.cpp
+++ src/ssh.cpp
@@ -4,15 +4,22 @@
 int main() {
 
     SSHClient client;
-    std::string u = "adsouza";
-    std::string p = "TrapServer123";
+    std::string u;
+    std::string p;
 
     try {
         client = SSHClient("aaron-tc");
         client.serverConnect();
 
+        std::cout << "Username: ";
+        std::cin >> u;
+
+        std::cout << "Password: ";
+        std::cin >> p;
 
         client.AuthenticateUser(u, p);
+
+        client.StartTerminal();
     }
     catch (const std::exception& e) {
         std::cerr << "Construction failed: " << e.what() << "\n";
